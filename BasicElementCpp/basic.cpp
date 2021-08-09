2d58d6f22c9eafa760ef63f501e170871f2a5525
widjoyono <71638181+widjoyono@users.noreply.github.com>
2021-08-09 16:46:52 +0700

    adding basic element 2

---
+++ BasicElementCpp/basic.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main () {
+	// declare everything
+	
+	int feet;
+	int inches;
+	
+	cout << " Enter two integers separated by one or more space ";
+	cin >> feet >> inches;
+	cout << endl;
+	
+	cout << " Feet: " << feet << endl;
+	cout << " Inches: " << inches << endl;
+	
+	return 0;
+	
+}
