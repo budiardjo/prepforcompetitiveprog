886ae1c957f0393016d8d5f7acb1b4fe7563a7ba
widjoyono <71638181+widjoyono@users.noreply.github.com>
2021-08-27 10:42:18 +0700

    just silly around

--- BasicElementCpp/basic1.cpp
+++ BasicElementCpp/basic1.cpp
@@ -1,18 +1,23 @@
-#include <iostream> //Line 1
-using namespace std; //Line 2
-const int NUMBER = 12; //Line 3
-int main() //Line 4
-{ //Line 5
-	int firstNum; //Line 6
-	int secondNum; //Line 7
-	firstNum = 18; //Line 8
-	cout << "Line 9: firstNum = " << firstNum << endl; //Line 9
-	cout << "Line 10: Enter an integer: "; //Line 10
-	cin >> secondNum; //Line 11
-	cout << endl; //Line 12
-	cout << "Line 13: secondNum = " << secondNum << endl; //Line 13
-	firstNum = firstNum + NUMBER + 2 * secondNum; //Line 14
-	cout << "Line 15: The new value of " << "firstNum = " << firstNum << endl; //Line 15
-
-	return 0; //Line 16
+/***************************************************************
+* A program that prints a boxed greeting using a void function *
+***************************************************************/
+#include <iostream>
+using namespace std;
+/***************************************************************
+* Function definition for the greeting function. This is a void *
+* function with no parameters that creates a three-line message. *
+* The function returns nothing to its caller. It has only a side *
+* effect (displaying three lines) *
+***************************************************************/
+void greeting ( )
+{
+cout <<"*******************" << endl;
+cout <<"* Hello Friends *" << endl;
+cout <<"*******************" ;
+return;
+}
+int main ( )
+{
+greeting(); // Calling the greeting function (expression statement)
+return 0;
 }
